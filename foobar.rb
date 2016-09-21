51460bcfb8d984a45e5279d0a4b93ff93caab2ed
Gabrielle <gbalan0n@users.noreply.github.com>
2016-09-20 23:29:53 -0700

    These are my files for HW1.

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,12 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    reg = a.map { |str| str.to_i }
+    cat = reg.map { |num| num + 2 }
+    meow = cat.select { |item| item % 2 == 0 }
+    animal = meow.uniq
+    dog = animal.select { |item| item < 10 }
+    sum = dog.inject(0){ |sum, x| sum + x }
+    return sum
   end
 end
-
-
